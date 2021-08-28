4cf74a4d86ac7f61917b4131eb1e718f81cc630a
venu <venubollepalli1@gmail.com>
2021-08-28 15:46:49 -0700

    materil ui

--- solarapp/src/App.js
+++ solarapp/src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-
+import AppBar from '@material-ui/core/AppBar';
+import Train from '@material-ui/icons/Train';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <>
+   <AppBar position="relative">
+
+<Toolbar>
+
+  <Train />
+
+  <Typography variant="h6" color="inherit" noWrap>
+
+   Metro Transit
+
+  </Typography>
+
+</Toolbar>
+
+</AppBar>
+    
+  
+
+
+
+  </>;
 }
 
 export default App;
